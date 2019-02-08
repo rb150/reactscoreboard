ea07e6634074160af0b136ef2c77de94b484994b
Work Horse <workhorse@Apples-MacBook-Pro.local>
2019-02-08 03:55:03 -0600

    score:2

--- score.jsx
+++ score.jsx
@@ -1,7 +1,45 @@
 function Application () {
     return (
-        <div className="application">
+        <div className="scoreboard">
+            {/*header*/}
+            <div className="header">
+                <h1>Scoreboard</h1>
+            </div>
+            {/*players, scores, plus/minus*/}
+            <div className="players">
+                {/*player*/}
+                <div className="player">
+                    <div className="player-name">
+                        Awesome awesomeson
+                    </div>
+                    {/*scores*/}
+                    <div className="player-score">
+                        {/*counter*/}
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score">31</div>
+                            <button className="counter-action increment"> + </button>   
+                        </div>
+                    </div>
+                </div>
 
+                {/*player*/}
+                <div className="player">
+                    <div className="player-name">
+                        Awesome awesomeson
+                    </div>
+                    {/*scores*/}
+                    <div className="player-score">
+                        {/*counter*/}
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score">31</div>
+                            <button className="counter-action increment"> + </button>   
+                        </div>
+                    </div>
+                </div>              
+                
+            </div>
         </div>
 
     );
