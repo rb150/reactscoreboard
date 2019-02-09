ff001ee8d72a01c1fb4bc79f7d58684b641e28e9
rb150 <rbajaj852@gmail.com>
2019-02-09 13:37:09 -0600

    score4

--- score.jsx
+++ score.jsx
@@ -1,10 +1,52 @@
-function Application () {
+function Application (props) {
     return (
-        <div>
+        <div className="scoreboard">
+            {/*header*/}
+            <div className="header">
+                <h1>{props.title}</h1>
+            </div>
+            {/*players, scores, plus/minus*/}
+            <div className="players">
+                {/*player*/}
+                <div className="player">
+                    <div className="player-name">
+                        Awesome awesomeson
+                    </div>
+                    {/*scores*/}
+                    <div className="player-score">
+                        {/*counter*/}
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score">31</div>
+                            <button className="counter-action increment"> + </button>   
+                        </div>
+                    </div>
+                </div>
 
+                {/*player*/}
+                <div className="player">
+                    <div className="player-name">
+                        Awesome awesomeson
+                    </div>
+                    {/*scores*/}
+                    <div className="player-score">
+                        {/*counter*/}
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score">31</div>
+                            <button className="counter-action increment"> + </button>   
+                        </div>
+                    </div>
+                </div>              
+                
+            </div>
         </div>
 
     );
 }
 
-ReactDOM.render(<Application />, document.getElementById("container"));
+Application.propTypes = {
+    title: React.PropTypes.string, //defines propTypes as with lower case p, React definition is in upper case
+}
+
+ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById("container"));
