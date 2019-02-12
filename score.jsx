e2212edb683fe80e6a9b1f8991f6b0a604e2f336
rb150 <rbajaj852@gmail.com>
2019-02-11 22:11:29 -0600

    score8

--- score.jsx
+++ score.jsx
@@ -1,3 +1,48 @@
+/*score1: add class with "className" attribute. class is reserved word in javascript
+which jsx gets compiled into, so "className" is used. Quotes are required */
+
+/*score2: addition of other elements to make our skeleton for our application
+In React className not used to retrieve elements from the page. Mainly used for styling.
+If div with no styling, className may not be necessary*/
+
+/*score3: addition of properties. Components (Application function in this case) take an argument.
+Argument is an object that takes in all the properties used when the instance of component was written out
+Property (title="My Scoreboard") added in ReactDOM.render. 
+To use roperty, wrap in curly braces otherwise it will be treated as text*/
+
+/*score4: addition of propTypes property. propTypes are application specific object that contains all
+the keys that a component can take, as well as a special type operation
+  score4.1: use .isRequired to require certain properties
+  score4.2: give a property a default value using "defaultProps" */
+
+/*score5: partialization of components into smaller components.
+Create function for partial (Header) and insert partial in main application by using
+self-closing tag.  If there are props (properties), add them in tag.
+  score5.1: made function for player (Player) component.  Added propTypes name (string) and score (number)
+  score5.2: made function for counter component (Counter) and moves counter and score outside macro component */
+
+/*score6: adds array with list of players in object form.  Must update propTypes in application to allow
+for (and require) array.  Use arrayOf() instead of array.  arrayOf() takes argument that is the type of object in array
+Instead of object, use shape() method.  Like arrayOf(), shape() takes argument that describes shape of object. This object
+looks like propType definition using keys and values
+In order to add players array to component, use .map() method on props.players (no for loops in react)
+map() method takes function which will take each player in player array. Return what should be rendered for each player
+  score6.1: adds keys to absolve: "Warning: Each child in an array or iterator should have a unique "key" prop" 
+  Keys help react know which items in loop are being re-arranged, added, or deleted. Key must be unique.  
+  Helps react recognize when it is being re-ordered so it can re-order each node instead of entire list
+  Id keys must be added to application propTypes and added in Player component (key=player.id) in Application component
+
+score7: Convert counter component to component class to allow for state (element that changes over time, as opposed to stateless).
+Object that createClass takes is an object that will define the methods in our class.  Some methods are in React for speacial purposes, but we can add our own.
+Must use this.props.score instead of props.score.  In a component class, props is actually a property of the class itself.
+it does not get passed into the render function like it does in a stateless functional component.
+Put propTypes in component class. When doing component classes, it is easier to put propTypes in component class 
+
+score8: Use state management mechanism in React (not as good as state management tools from the community).
+There are libraries to help manage state (flux, redux, etc.).
+Create getInitialState() method to set counter state.  Add to counter class.  Replace "props. in this.props.score with "state"
+Since we don't need props, remove from propTypes (can still keep object empty). Update player function to no longer pass score in <Counter/>*/
+
 var PLAYERS = [
     {
         name: "someone",
@@ -32,18 +77,6 @@ Header.propTypes = {
 var Counter = React.createClass({
     propTypes: {},
 
-    incrementScore: function(e) {
-        this.setState({
-            score : (this.state.score + 1)
-        })
-    },
-
-    decrementScore: function(e) {
-        this.setState({
-            score : (this.state.score - 1)
-        })
-    },
-
     getInitialState: function() {
         return {
             score: 0,
@@ -54,9 +87,9 @@ var Counter = React.createClass({
     return (
         //Counter
         <div className="counter">
-            <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+            <button className="counter-action decrement"> - </button>
             <div className="counter-score">{this.state.score}</div>
-            <button className="counter-action increment" onClick={this.incrementScore}> + </button>   
+            <button className="counter-action increment"> + </button>   
         </div>
     );
    } 
